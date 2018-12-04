83d14eb898394f30964761e59e650087ad5b7445
BerensF <44698085+BerensF@users.noreply.github.com>
2018-12-04 12:49:09 +0100

    Update random_points.h

--- LowerBounds/random_points.h
+++ LowerBounds/random_points.h
@@ -15,13 +15,37 @@ using namespace std;
 
 vector<unsigned int> random_points(unsigned int number_of_all_points, unsigned int number_to_selected)
 {
+	/*
+		random_points:
+		Samples randomly number_to_selected integers from range 0 to number_of_all_points.
+		
+		Input 
+		-----
+		number_of_all_points - an int, which stands for the number of points on the isosurface
+		number_to_selected - an int, which stands for the number of points that should be selected
+		
+		Output
+		------
+		Is a vector of size number_to_selected, filled with int
+		
+		Complexity
+    		----------
+		O(number_of_all_points)
+		
+		Example
+		-------
+		vector <int> A = random_points(10, 3);
+		cout << A[0] << " " << A[1] << " " << A[2];
+		
+		=> 2 0 4
+	*/
 	if(number_of_all_points < number_to_selected)
 	{
 		cerr << "Can't select more points, then there are. Number of all points: " << number_of_all_points << " Number of selected points: " << number_to_selected;
 		exit(-1);
 	}
 
-	vector<unsigned int> list;
+	vector<unsigned int> list_of_numbers;
 	vector<unsigned int> out;
 	unsigned int random_number;
 
